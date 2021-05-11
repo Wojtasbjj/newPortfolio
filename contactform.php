38cf1c49bf284314f3cdbba8c04bb21683eeee24
WojtasBjj <wojtekgajda94@gmail.com>
2021-05-11 16:51:25 +0200

    added php for sending messages

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "wojtekgajda94@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recived an e-mail from: ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
