21c567b15e2787f164765920e151ab9bd11d4a84
WojtasBjj <wojtekgajda94@gmail.com>
2021-05-12 15:17:23 +0200

    added footer

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ Vue.createApp({
     data() {
         return {
             mobileNav: false,
+            showTel: false
         }
     },
     methods: {
         test() {
             this.mobileNav = !this.mobileNav;
+        },
+        sendEmail() {
+            console.log('wyslano wiadomosc')
         }
     }
-}).mount('.navbar')
+}).mount('body')
